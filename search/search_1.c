1d0ac7328f6c40288c99b1762dfe82f82ec79725
jaesoon-park <wotns2606@cu.ac.kr>
2019-11-25 16:09:33 +0900

    2019_11_25algorithms

---
+++ search/search_1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNIGS
+#include <stdio.h>
+
+
+int main()
+{
+	int se[100];
+	int x;
+	int ta;
+
+	for (int i = 0; i < x; i++) {
+		if (se[i] == ta)
+			return i;
+		return -1;
+	}
+}
