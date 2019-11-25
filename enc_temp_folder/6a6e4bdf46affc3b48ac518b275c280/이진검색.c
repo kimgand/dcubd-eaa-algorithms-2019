66b4d7048ca890341e5859f2ff9589071a6ef673
yegyun <yk6489@cu.ac.kr>
2019-11-25 16:09:41 +0900

    비교과

---
+++ enc_temp_folder/6a6e4bdf46affc3b48ac518b275c280/이진검색.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int Search() {
+	
+
+}
+int main() {
+	int a[256];
+	int n,pl, pc, pr;
+	printf("오름차순으로 배열을 쓰시오");
+	printf("배열 개수 : ");
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++) {
+		printf("a[%d]=", i);
+		scanf("%d", &a[i]);
+	}
+	
+
+	pl = a[0];
+
+}
